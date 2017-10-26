af9c59e4fbaf2112ffaafcd78548e76d6b637318
Semi Turdean <Semiturdean@hotmail.com>
2017-10-26 13:08:04 +0200

    upd

--- Uppgift3OOP/src/Button.java
+++ Uppgift3OOP/src/Button.java
@@ -2,15 +2,15 @@
 public class Button {
 
 	Button(){
-<<<<<<< HEAD
+
 		System.out.println("Hej Richard");
 		//Hej
-=======
+
 		
 		System.out.println("Richard testar branch1");
 		System.out.println("testar en till branch merge");
 
->>>>>>> branch 'master' of https://github.com/RichardHags/Uppgift3OOP.git
+
 	}
 	
 }
