b8dfbcab7912d6ea53fa829c3a82a30c962be029
Semi Turdean <Semiturdean@hotmail.com>
2017-10-26 18:50:42 +0200

    update

--- Uppgift3OOP/src/Frame.java
+++ Uppgift3OOP/src/Frame.java
@@ -1,6 +1,34 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Frame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Frame extends JFrame implements ActionListener {
+	
+
+	private final JPanel gamePanel = new JPanel();
+	private final JPanel buttonPanel = new JPanel();
+	private final JButton newGame = new JButton("New game");
+	private final JButton endGame = new JButton("End game");
+	
 	public Frame() {
-		System.out.println("Hej");
+		
+		// panelLayout etc
+		
+		setLayout(new BorderLayout());
+		add(gamePanel, BorderLayout.NORTH);
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		buttonPanel.add(newGame); buttonPanel.add(endGame);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
