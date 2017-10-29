81c002337c22063a77d857afb783b9d534381f9e
Richard <richardhags@gmail.com>
2017-10-29 14:33:42 +0100

    Enkel knapp klass. håller reda på row/col

--- Uppgift3OOP/src/Button.java
+++ Uppgift3OOP/src/Button.java
@@ -1,4 +1,26 @@
 
 public class Button {
+	
+	protected int row;
+	protected int col;
+	protected String name;
+	
+	public Button(String name, int row, int col) {
+		this.name = name;
+		this.row = row;
+		this.col = col;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	
+	
+	
 
 }
